f110f9b0d0e5b3bcb95c0bd73367a19e7882aabf
Simon Vandamme <simon.vandamme@skyline.be>
2022-11-21 18:11:50 +0100

    sonarqube 2
    Change-Id: I6a0d94b74dd70f334b5fcc742f88c8dbdc2ea3af

--- InterfaceUtilizationHelperTests/InterfaceUtilizationHelperTests.cs
+++ InterfaceUtilizationHelperTests/InterfaceUtilizationHelperTests.cs
@@ -3,69 +3,69 @@
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 	[TestClass]
-    public class InterfaceUtilizationHelperTests
-    {
-        [TestMethod]
-        public void CalculateUtilizationTest_InvalidInputRate()
-        {
+	public class InterfaceUtilizationHelperTests
+	{
+		[TestMethod]
+		public void CalculateUtilizationTest_InvalidInputRate()
+		{
 			double expected = -1;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(-1, 2000, 5000, DuplexStatus.FullDuplex);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_InvalidOutputRate()
+		[TestMethod]
+		public void CalculateUtilizationTest_InvalidOutputRate()
 		{
 			double expected = -1;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, -1, 5000, DuplexStatus.FullDuplex);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_InvalidSpeed()
+		[TestMethod]
+		public void CalculateUtilizationTest_InvalidSpeed()
 		{
 			double expected = -1;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, 2000, 0, DuplexStatus.FullDuplex);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_InvalidDuplexStatus_NA()
+		[TestMethod]
+		public void CalculateUtilizationTest_InvalidDuplexStatus_NA()
 		{
 			double expected = -1;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, 2000, 0, DuplexStatus.NotInitialized);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_InvalidDuplexStatus_Unknown()
+		[TestMethod]
+		public void CalculateUtilizationTest_InvalidDuplexStatus_Unknown()
 		{
 			double expected = -1;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, 2000, 0, DuplexStatus.Unknown);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_HalfDuplex()
+		[TestMethod]
+		public void CalculateUtilizationTest_HalfDuplex()
 		{
 			double expected = 70;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, 2000, 5000, DuplexStatus.HalfDuplex);
 
 			Assert.AreEqual(expected, result);
-        }
+		}
 
-        [TestMethod]
-        public void CalculateUtilizationTest_FullDuplex()
+		[TestMethod]
+		public void CalculateUtilizationTest_FullDuplex()
 		{
 			double expected = 40;
 			double result = InterfaceUtilizationHelper.CalculateUtilization(1500, 2000, 5000, DuplexStatus.FullDuplex);
 
 			Assert.AreEqual(expected, result);
-        }
-    }
+		}
+	}
 }
